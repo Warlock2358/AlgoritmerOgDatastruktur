936beb857af6b5bd60ed8dc0643f51ce24789d50
Isak Kallestad Mandal <63557128+Warlock2358@users.noreply.github.com>
2023-10-31 22:50:08 +0100

    Removed redundant methods and added method to use example code

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -14,30 +14,11 @@ public class HuffmanTree {
     this.nodes = nodes;
   }
 
-//  public void buildTree() {
-//    while (nodes.size() > 1) {
-//      HuffmanNode minNode1 = nodes.stream().min(Comparator.comparingInt(HuffmanNode::getFrequency)).get();
-//      nodes.remove(minNode1);
-//      HuffmanNode minNode2 = nodes.stream().min(Comparator.comparingInt(HuffmanNode::getFrequency)).orElse(null);
-//      nodes.remove(minNode2);
-//
-//      HuffmanNode f = new HuffmanNode();
-//
-//      if (minNode2 != null) {
-//        f.frequency = minNode1.frequency + minNode2.frequency;
-//      }
-//
-//      f.left = minNode1;
-//
-//      f.right = minNode2;
-//
-//      root = f;
-//
-//      nodes.add(f);
-//    }
-//  }
+  public HuffmanTree() {
+  }
 
   public HuffmanNode generateTree(PriorityQueue<HuffmanNode> pq) {
+
     while (pq.size() > 1) {
       HuffmanNode left = pq.poll();
       HuffmanNode right = pq.poll();
@@ -58,56 +39,75 @@ public class HuffmanTree {
     }
     return pq.poll();
   }
-
-
-  public void printTree(HuffmanNode node, String s, ArrayList<HuffmanCode> hc) {
-    if (node.left == null && node.right == null && Character.isDefined(node.c)) {
-      //System.out.println(node.c + ":" + s);
-      HuffmanCode huffmanCode;
-      huffmanCode = new HuffmanCode();
-      huffmanCode.c = node.c;
-      huffmanCode.code = s;
-      node.code = s;
-      hc.add(huffmanCode);
-      return;
-    }
-    if (!Character.isDefined(node.c)){
-      System.out.println(node.c);
-    }
-    assert node.left != null;
-    printTree(node.left, s + "0", hc);
-    printTree(node.right, s + "1", hc);
-  }
-
   public void encodeString(String s, ArrayList<HuffmanCode> hc) {
     for (char c : s.toCharArray()) {
       encodedString.append(Objects.requireNonNull(hc.stream().filter(huffmanCode -> huffmanCode.c == c).findFirst().orElse(null)).code);
     }
   }
+  public void createHuffmanCodes(HuffmanNode root, String code, ArrayList<HuffmanCode> hc) {
+    if (root == null) {
+      return;
+    }
 
-  public String getEncodedString() {
-    return encodedString.toString();
+    if (root.isLeaf()) {
+      hc.add(new HuffmanCode(root.c, code));
+      root.code = code;
+    }
+
+    createHuffmanCodes(root.left, code + "0", hc);
+    createHuffmanCodes(root.right, code + "1", hc);
   }
 
-  public void decodeString(String encodedString, HuffmanNode root) {
-    HuffmanNode node = root;
-    for (char c : encodedString.toCharArray()) {
-      if (c == '0') {
-        node = node.left;
-      } else {
-        node = node.right;
-      }
 
-      assert node != null;
-      if (node.left == null && node.right == null) {
-        //System.out.print(node.c);
-        decodedString.append(node.c);
-        node = root;
+  public HuffmanNode buildHuffmanTree(int[] frequencies) {
+    PriorityQueue<HuffmanNode> pq = buildPriorityQueue(frequencies) ;
+
+    while (pq.size() > 1) {
+      HuffmanNode left = pq.poll();
+      HuffmanNode right = pq.poll();
+
+      HuffmanNode node = new HuffmanNode(left, right);
+      assert right != null;
+      node.frequency = left.frequency + right.frequency;
+      node.left = left;
+      node.right = right;
+
+      pq.add(node);
+    }
+
+    return pq.poll();
+  }
+
+  public PriorityQueue<HuffmanNode> buildPriorityQueue(int[] frequencies) {
+    PriorityQueue<HuffmanNode> pq = new PriorityQueue<>();
+
+    for (int i = 0; i < frequencies.length; i++) {
+      if (frequencies[i] > 0) {
+        char c = (char) i;
+        int frequency = frequencies[i];
+        HuffmanNode node = new HuffmanNode(c, frequency);
+        pq.add(node);
       }
     }
+    return pq;
   }
 
-  public String getDecodedString() {
-    return decodedString.toString();
+  public void generateHuffmanCodes(HuffmanNode root, String code, String[] huffmanCode) {
+    if (root == null) {
+      return;
+    }
+
+    if (root.isLeaf()) {
+      huffmanCode[root.c] = code;
+    }
+
+    generateHuffmanCodes(root.left, code + "0", huffmanCode);
+    generateHuffmanCodes(root.right, code + "1", huffmanCode);
+  }
+
+  public String[] getHuffmanCodes(HuffmanNode root) {
+    String[] huffmanCodes = new String[256];
+    generateHuffmanCodes(root, "", huffmanCodes);
+    return huffmanCodes;
   }
 }
